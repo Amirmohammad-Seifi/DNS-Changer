37df931301f0b30abee2252907bf34e772e18a5a
Amirmohammad-Seifi <amirmohammadseifi365@gmail.com>
2025-06-04 21:57:19 +0330

    Update main.py
    mistake commit fixed :)

--- main.py
+++ main.py
@@ -28,69 +28,127 @@ print("""DNS servers list:
      ----------------------------------------
      """)
 sd = input('select dns: ')  # 'sd' -> selected DNS
-    
-cnc = subprocess.run(['ipconfig', '/all'], capture_output=True).stdout.decode()  # 'cnc' -> cmd network check
-cenf = list(re.findall('Ethernet adapter (.*):', cnc))  # 'cenf' -> cmd ethernet network find
-# cwnf = list(re.findall('Wireless LAN adapter (.*):', cnc))  # 'cwnf' -> cmd wifi network find
 if sd == 'F' or 'f':
-    cfd = subprocess.run(['ipconfig', '/flushdns'])  # 'cfd' -> Cmd flush DNS
+    uo = input("""    ___________________________
+    | OS name |   OS number   |
+    |-------------------------|
+    |  Linux  |      (-)      |
+    |-------------------------|
+    | Windows |      (2)      |
+    |-------------------------|
+    |   Mac   |      (-)      |
+    |-------------------------|
+
+select your os: """)  # 'uo' -> user os
 else:
-    num = 0
-    nl = []  # 'nl' -> Network list
-    head = ('    ' + 71 * '_')
-    body = ('    |' + 69 * '-' + '|')
-    print(head, '    |' + (18 * ' ') + 'Network name' + (18 * ' ') + '|   Network number   |', body, sep='\n')
-    for network in cenf:
-        num += 1
-        nn = (int(abs(float(len(network) - 48) // 2)) * ' ' + f'{network}' + int(abs(float(len(network) - 48) / 2)) * ' ' )  # 'nn' -> Network name
-        nc = (f'        ({num})         ')  # 'nc' -> Network code
-        # print('    |' + int(abs(float(len(network) - 48) // 2)) * ' ' + f'{network}' + int(abs(float(len(network) - 48) / 2)) * ' ' + '|' + f'        ({num})         |', body, sep='\n')
-        print('    |' + nn + '|' + nc + '|', body, sep='\n')
-        nl.append(network)
+    uo = input("""    ___________________________
+    | OS name |   OS number   |
+    |-------------------------|
+    |  Linux  |      (1)      |
+    |-------------------------|
+    | Windows |      (2)      |
+    |-------------------------|
+    |   Mac   | Coming soon...|
+    |-------------------------|
 
-    # for network in cwnf:
-    #     num += 1
-    #     nn = (int(abs(float(len(network) - 48) // 2)) * ' ' + f'{network}' + int(abs(float(len(network) - 48) / 2)) * ' ' )  # 'nn' -> Network name
-    #     nc = (f'        ({num})         ')  # 'nc' -> Network code
-    #     # print('    |' + int(abs(float(len(network) - 48) // 2)) * ' ' + f'{network}' + int(abs(float(len(network) - 48) / 2)) * ' ' + '|' + f'        ({num})         |', body, sep='\n')
-    #     print('    |' + nn + '|' + nc + '|', body, sep='\n')
-    #     nl.append(network)
-    ns = int(input('\nselect your network: '))  # 'ns' -> Network select
+select your os: """)
+if uo == '1':  # Linux section
+    if sd == '1':
+        os.system(f'echo nameserver {cf_t[1]} > /etc/resolv.conf && echo nameserver {cf_t[2]} >> /etc/resolv.conf')
+    elif sd == '2':
+        os.system(f'echo nameserver {g_t[1]} > /etc/resolv.conf && echo nameserver {g_t[2]} >> /etc/resolv.conf')
+    elif sd == '3':
+        os.system(f'echo nameserver {od_t[1]} > /etc/resolv.conf && echo nameserver {od_t[2]} >> /etc/resolv.conf')
+    elif sd == '4':
+        os.system(f'echo nameserver {sh_t[1]} > /etc/resolv.conf && echo nameserver {sh_t[2]} >> /etc/resolv.conf')
+    elif sd == '5':
+        os.system(f'echo nameserver {a_t[1]} > /etc/resolv.conf && echo nameserver {a_t[2]} >> /etc/resolv.conf')
+    else:
+        print('please type your dns server code.')
+    udl = subprocess.run(['cat', '/etc/resolv.conf'], capture_output=True).stdout.decode()  # 'udl' -> user dns linux
+    udlf = tuple(re.findall('nameserver (.*)', udl))  # 'udlf' -> user dns linux find
+    if udlf == cf_t[1:]:
+        print(f"You are now using the '{cf_t[0]}' DNS \nGood luck!")
+        time.sleep(2)
+        os.system('clear')
+    elif udlf == g_t[1:]:
+        print(f"You are now using the '{g_t[0]}' DNS \nGood luck!")
+        time.sleep(2)
+        os.system('clear')
+    elif udlf == od_t[1:]:
+        print(f"You are now using the '{od_t[0]}' DNS \nGood luck!")
+        time.sleep(2)
+        os.system('clear')
+    elif udlf == sh_t[1:]:
+        print(f"You are now using the '{sh_t[0]}' DNS \nGood luck!")
+        time.sleep(2)
+        os.system('clear')
+    elif udlf == a_t[1:]:
+        print(f"You are now using the '{a_t[0]}' DNS \nGood luck!")
+        time.sleep(2)
+        os.system('clear')
+elif uo == '2':  # Windows section
+    cnc = subprocess.run(['ipconfig', '/all'], capture_output=True).stdout.decode()  # 'cnc' -> cmd network check
+    cenf = list(re.findall('Ethernet adapter (.*):', cnc))  # 'cenf' -> cmd ethernet network find
+    cwnf = list(re.findall('Wireless LAN adapter (.*):', cnc))  # 'cwnf' -> cmd wifi network find
+    if sd == 'F' or 'f':
+        cfd = subprocess.run(['ipconfig', '/flushdns'])  # 'cfd' -> Cmd flush DNS
+    else:
+        num = 0
+        nl = []  # 'nl' -> Network list
+        head = ('    ' + 71 * '_')
+        body = ('    |' + 69 * '-' + '|')
+        print(head, '    |' + (18 * ' ') + 'Network name' + (18 * ' ') + '|   Network number   |', body, sep='\n')
+        for network in cenf:
+            num += 1
+            nn = (int(abs(float(len(network) - 48) // 2)) * ' ' + f'{network}' + int(abs(float(len(network) - 48) / 2)) * ' ' )  # 'nn' -> Network name
+            nc = (f'        ({num})         ')  # 'nc' -> Network code
+            # print('    |' + int(abs(float(len(network) - 48) // 2)) * ' ' + f'{network}' + int(abs(float(len(network) - 48) / 2)) * ' ' + '|' + f'        ({num})         |', body, sep='\n')
+            print('    |' + nn + '|' + nc + '|', body, sep='\n')
+            nl.append(network)
 
-if sd == '1':
-    cpds = os.system(f'netsh interface ipv4 set dns "{nl[ns-1]}" static {cf_t[1]}')  # 'cpds' -> cmd primary DNS set
-    csds = os.system(f'netsh interface ipv4 add dns "{nl[ns-1]}" {cf_t[2]} index=2')  # 'csds' -> cmd secondary DNS set
-    print(f"You are now using the '{cf_t[0]}' DNS \nGood luck!")
-    time.sleep(2)
-    os.system('cls')
-elif sd == '2':
-    cpds = os.system(f'netsh interface ipv4 set dns "{nl[ns-1]}" static {g_t[1]}')
-    csds = os.system(f'netsh interface ipv4 add dns "{nl[ns-1]}" {g_t[2]} index=2')
-    print(f"You are now using the '{g_t[0]}' DNS \nGood luck!")
-    time.sleep(2)
-    os.system('cls')
-elif sd == '3':
-    cpds = os.system(f'netsh interface ipv4 set dns "{nl[ns-1]}" static {od_t[1]}')
-    csds = os.system(f'netsh interface ipv4 add dns "{nl[ns-1]}" {od_t[2]} index=2')
-    print(f"You are now using the '{od_t[0]}' DNS \nGood luck!")
-    time.sleep(2)
-    os.system('cls')
-elif sd == '4':
-    cpds = os.system(f'netsh interface ipv4 set dns "{nl[ns-1]}" static {sh_t[1]}')
-    csds = os.system(f'netsh interface ipv4 add dns "{nl[ns-1]}" {sh_t[2]} index=2')
-    print(f"You are now using the '{sh_t[0]}' DNS \nGood luck!")
-    time.sleep(2)
-    os.system('cls')
-elif sd == '5':
-    cpds = os.system(f'netsh interface ipv4 set dns "{nl[ns-1]}" static {a_t[1]}')
-    csds = os.system(f'netsh interface ipv4 add dns "{nl[ns-1]}" {a_t[2]} index=2')
-    print(f"You are now using the '{a_t[0]}' DNS \nGood luck!")
-    time.sleep(2)
-    os.system('cls')
-elif sd == 'd' or 'D':
-    cpds = os.system(f'netsh interface ipv4 set dns "{nl[ns-1]}" dhcp')
+        for network in cwnf:
+            num += 1
+            nn = (int(abs(float(len(network) - 48) // 2)) * ' ' + f'{network}' + int(abs(float(len(network) - 48) / 2)) * ' ' )  # 'nn' -> Network name
+            nc = (f'        ({num})         ')  # 'nc' -> Network code
+            # print('    |' + int(abs(float(len(network) - 48) // 2)) * ' ' + f'{network}' + int(abs(float(len(network) - 48) / 2)) * ' ' + '|' + f'        ({num})         |', body, sep='\n')
+            print('    |' + nn + '|' + nc + '|', body, sep='\n')
+            nl.append(network)
+        ns = int(input('\nselect your network: '))  # 'ns' -> Network select
 
-elif sd == 'F' or 'f':
-    pass
-else:
-    print('please type your dns server code.')
+    if sd == '1':
+        cpds = os.system(f'netsh interface ipv4 set dns "{nl[ns-1]}" static {cf_t[1]}')
+        csds = os.system(f'netsh interface ipv4 add dns "{nl[ns-1]}" {cf_t[2]} index=2')
+        print(f"You are now using the '{cf_t[0]}' DNS \nGood luck!")
+        time.sleep(2)
+        os.system('cls')
+    elif sd == '2':
+        cpds = os.system(f'netsh interface ipv4 set dns "{nl[ns-1]}" static {g_t[1]}')
+        csds = os.system(f'netsh interface ipv4 add dns "{nl[ns-1]}" {g_t[2]} index=2')
+        print(f"You are now using the '{g_t[0]}' DNS \nGood luck!")
+        time.sleep(2)
+        os.system('cls')
+    elif sd == '3':
+        cpds = os.system(f'netsh interface ipv4 set dns "{nl[ns-1]}" static {od_t[1]}')
+        csds = os.system(f'netsh interface ipv4 add dns "{nl[ns-1]}" {od_t[2]} index=2')
+        print(f"You are now using the '{od_t[0]}' DNS \nGood luck!")
+        time.sleep(2)
+        os.system('cls')
+    elif sd == '4':
+        cpds = os.system(f'netsh interface ipv4 set dns "{nl[ns-1]}" static {sh_t[1]}')
+        csds = os.system(f'netsh interface ipv4 add dns "{nl[ns-1]}" {sh_t[2]} index=2')
+        print(f"You are now using the '{sh_t[0]}' DNS \nGood luck!")
+        time.sleep(2)
+        os.system('cls')
+    elif sd == '5':
+        cpds = os.system(f'netsh interface ipv4 set dns "{nl[ns-1]}" static {a_t[1]}')
+        csds = os.system(f'netsh interface ipv4 add dns "{nl[ns-1]}" {a_t[2]} index=2')
+        print(f"You are now using the '{a_t[0]}' DNS \nGood luck!")
+        time.sleep(2)
+        os.system('cls')
+    elif sd == 'd' or 'D':
+        cpds = os.system(f'netsh interface ipv4 set dns "{nl[ns-1]}" dhcp')
+    elif sd == 'F' or 'f':
+        pass
+    else:
+        print('please type your dns server code.')
